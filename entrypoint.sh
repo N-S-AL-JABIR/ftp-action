f844876697c8b649fb3cbca5f74d660436faeda3
Al Jabir <136284119+N-S-AL-JABIR@users.noreply.github.com>
2025-12-06 20:51:30 +0600

    Change mirror command to use -e option
    Remove Files not found in local mirror from remote

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@ lftp ${INPUT_HOST} -u ${INPUT_USER},${INPUT_PASSWORD} -e "
   set ftp:ssl-force $INPUT_FORCESSL; 
   set sftp:auto-confirm yes;
   set ssl:verify-certificate $INPUT_FORCESSL; 
-  mirror -v -P 5 -R -n -L -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
+  mirror -v -P 5 -R -e -n -L -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR;
   quit
 "
